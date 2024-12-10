782380a0e8c0af411ad0b15d76430cfba350eee3
Prashanth yejje <pyejje@bitbucket.allmysons.com>
2024-12-10 15:00:12 +0530

    i think

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Fridgepage from './stores/pages/fridgepage';
 
 import Tvpage from './stores/pages/Tvpage';
 import Speakerpage from './stores/pages/Speakerpage';
-import Fridgepage from './stores/pages/fridgepage'
+
 
 
 import Bookspage from './stores/pages/Bookspage';
